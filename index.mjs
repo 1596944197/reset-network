ad0eb436e00a0ba18e8ad551f3ec4aedb9de0c11
mei_yiyi <1596944197@qq.com>
2023-03-18 12:11:28 +0800

    feat:基础功能完成“

--- index.mjs
+++ index.mjs
@@ -2,7 +2,14 @@ const { exec } = require("child_process");
 
 
 const command = `
-
+netsh int tcp set heuristics disabled
+netsh int tcp set global autotuninglevel=disabled
+netsh int tcp set global rss=enabled
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
 `
 
 exec(command, (error, stdout, stderr) => {
@@ -14,5 +21,9 @@ exec(command, (error, stdout, stderr) => {
     console.log(`stderr: ${stderr}`);
     return;
   }
-  console.log(`stdout: ${stdout}`);
+  console.log(`已重置网络: ${stdout}`);
 });
+
+/** @returns {https://answers.microsoft.com/zh-hans/windows/forum/all/0x800704cf/0949346c-ed7c-40d6-a72d-3dd2fd3d0306} */
+
+
