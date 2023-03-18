f683862fe3142b3e88f627aa7634e7db324afac3
mei_yiyi <1596944197@qq.com>
2023-03-18 12:20:08 +0800

    feat:promise化，并增加文档

---
+++ README.md
@@ -0,0 +1,16 @@
+# 用于重置网络
+
+### how to install
+
+```
+npm i reset-network
+```
+
+### how to use
+
+```
+import { reset } from 'reset-network'
+
+// is a promise
+reset()
+```
--- index.mjs
+++ index.mjs
@@ -2,27 +2,31 @@ const { exec } = require("child_process");
 
 
 const command = `
-netsh int tcp set heuristics disabled
-netsh int tcp set global autotuninglevel=disabled
-netsh int tcp set global rss=enabled
-netsh winsock reset
-netsh int ip reset
-ipconfig /release
-ipconfig /renew
-ipconfig /flushdns
+netsh int tcp set heuristics disabled &&
+netsh int tcp set global autotuninglevel=disabled &&
+netsh int tcp set global rss=enabled &&
+netsh winsock reset &&
+netsh int ip reset &&
+ipconfig /release &&
+ipconfig /renew &&
+ipconfig /flushdns &&
 `
 
-exec(command, (error, stdout, stderr) => {
-  if (error) {
-    console.log(`error: ${error.message}`);
-    return;
-  }
-  if (stderr) {
-    console.log(`stderr: ${stderr}`);
-    return;
-  }
-  console.log(`已重置网络: ${stdout}`);
-});
+export function reset() {
+  return new Promise((resolve, reject) => {
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        reject(`error: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        reject(`stderr: ${stderr}`);
+        return;
+      }
+      resolve(`已重置网络: ${stdout}`);
+    });
+  })
+}
 
 /** @returns {https://answers.microsoft.com/zh-hans/windows/forum/all/0x800704cf/0949346c-ed7c-40d6-a72d-3dd2fd3d0306} */
 
